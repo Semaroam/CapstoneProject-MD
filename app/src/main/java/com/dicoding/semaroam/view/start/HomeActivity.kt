e56c7738c7ec0f7e9cbad20b69ab99d29158a216
Indris2003 <indris2003@gmail.com>
2024-06-10 14:29:00 +0700

    update and new fitur

--- app/src/main/java/com/dicoding/semaroam/view/start/HomeActivity.kt
+++ app/src/main/java/com/dicoding/semaroam/view/start/HomeActivity.kt
@@ -1,12 +1,35 @@
 package com.dicoding.semaroam.view.start
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.semaroam.R
+import com.dicoding.semaroam.view.profil.ProfileActivity
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val profileButton: ImageButton = findViewById(R.id.et_profile)
+        val hiUserTextView: TextView = findViewById(R.id.tv_hi_user)
+
+        sharedPreferences = getSharedPreferences("user_pref", MODE_PRIVATE)
+
+        profileButton.setOnClickListener {
+            val intent = Intent(this@HomeActivity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Load user name from SharedPreferences and set it to the TextView
+        val userName = sharedPreferences.getString("user_name", "User")
+        hiUserTextView.text = getString(R.string.hi) + " " + userName
     }
 }
